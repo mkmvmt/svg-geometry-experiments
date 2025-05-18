278bc5e791d9bd0c4a2e6468327e4dd953f0ad0e
Alexis Trépanier <57072280+mkmvmt@users.noreply.github.com>
2025-05-18 08:54:55 -0400

    Create utils.js

---
+++ src/utils.js
@@ -0,0 +1,8 @@
+/**
+ * Convert degrees to radians
+ * @param {number} angle - Angle in degrees
+ * @returns {number} Angle in radians
+ */
+export function toRad(angle) {
+  return angle * Math.PI / 180;
+}
